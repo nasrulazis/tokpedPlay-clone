2a0fc2585f1f25c66f2870d0dd0890045495cdca
Mohammad Nasrul Azis <mohammadnasrulazis@gmail.com>
2023-08-15 06:30:56 +0700

    axios fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,11 @@ const URL = import.meta.env.VITE_BASE_URL
 
 const useAxios = axios.create({
     baseURL:URL,
+    headers: {
+        'Content-Type': 'application/json',
+      },
     withCredentials:true
 })
+
 
 export default useAxios;
